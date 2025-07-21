fee2aa702c4b64dc08d3b952aff228c4b9c7629e
Fardin Zahid <fardinx37@gmail.com>
2025-07-21 22:57:26 +0200

    Simplify vercel.json to fix build issues

--- src/pages/api/debug.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
-    NODE_ENV: process.env.NODE_ENV,
-    allEnvVars: Object.keys(process.env).filter(key => key.startsWith('NEXT_PUBLIC'))
-  });
-}
